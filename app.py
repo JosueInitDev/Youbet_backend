0fc31bd052fb34f2ef30ab544eb89175893871de
Darius <jose.init.dev@gmail.com>
2026-02-13 16:06:21 +0100

    app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from cron.fetch_fixtures import run as fetch_fixtures
 from cron.fetch_lives import fetch_lives
 from apscheduler.schedulers.background import BackgroundScheduler
 from cron.settle_pending_bets import settle_pending_bets
+import asyncio
 
 app = FastAPI(title="Youbet Backend", redirect_slashes=False)
 
@@ -24,20 +25,20 @@ app.include_router(leagues.router)
 scheduler = BackgroundScheduler()
 @app.on_event("startup")
 async def startup_event():
-    # print("[STARTUP] Fetching fixtures and live matches once...")
-    # loop = asyncio.get_event_loop()
+    print("[STARTUP] Fetching fixtures and live matches once...")
+    loop = asyncio.get_event_loop()
     
-    # try:
-    #     await loop.run_in_executor(None, fetch_fixtures)
-    # except Exception as e:
-    #     print(f"[ERROR] fetch_fixtures failed: {e}")
+    try:
+        await loop.run_in_executor(None, fetch_fixtures)
+    except Exception as e:
+        print(f"[ERROR] fetch_fixtures failed: {e}")
 
-    # try:
-    #     await loop.run_in_executor(None, fetch_lives)
-    # except Exception as e:
-    #     print(f"[ERROR] fetch_lives failed: {e}")
+    try:
+        await loop.run_in_executor(None, fetch_lives)
+    except Exception as e:
+        print(f"[ERROR] fetch_lives failed: {e}")
 
-    # print("[STARTUP] Done fetching initial data.")
+    print("[STARTUP] Done fetching initial data.")
 
     scheduler.add_job(fetch_lives, "interval", minutes=1, id="job1")
     scheduler.add_job(fetch_fixtures, "interval", minutes=1440, id="job2")
